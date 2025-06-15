863458767199c417185817930da699fbb03abb98
Dacosta99 <dyaluzan@unal.edu.co>
2025-06-15 13:43:02 -0500

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import ProductList from './ProductList';
 import './App.css';
 import AboutUs from './AboutUs';
-import { Provider } from 'react-redux';
-import store from './store';
 
 function App() {
   
@@ -18,31 +16,29 @@ function App() {
   };
 
   return (
-    <Provider store={store}>
-      <div className="app-container">
-        <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
-          <div className="background-image"></div>
-          <div className="content">
-           <div className="landing_content">
-           <h1>Welcome To Paradise Nursery</h1>
-            <div className="divider"></div>
-            <p>Where Green Meets Serenity</p>
-           
-            <button className="get-started-button" onClick={handleGetStartedClick}>
-              Get Started
-            </button>
-           </div>
-            <div className="aboutus_container">
-            <AboutUs/>
-            </div>
-            </div>
+    <div className="app-container">
+      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+        <div className="background-image"></div>
+        <div className="content">
+         <div className="landing_content">
+         <h1>Welcome To Paradise Nursery</h1>
+          <div className="divider"></div>
+          <p>Where Green Meets Serenity</p>
+         
+          <button className="get-started-button" onClick={handleGetStartedClick}>
+            Get Started
+          </button>
+         </div>
+          <div className="aboutus_container">
+          <AboutUs/>
+          </div>
+          </div>
 
-        </div>
-        <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
-          <ProductList onHomeClick={handleHomeClick}/>
-        </div>
       </div>
-    </Provider>
+      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+        <ProductList onHomeClick={handleHomeClick}/>
+      </div>
+    </div>
   );
 }
 
